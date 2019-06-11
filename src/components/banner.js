839a46bf38ffabb4b2c529173ad2a1a9c7536876
Jesse Martinez <jezze.04@gmail.com>
2019-06-11 15:22:44 -0500

    more updates to banner styling to make it more appealing

--- src/components/banner.js
+++ src/components/banner.js
@@ -6,23 +6,57 @@ import Typography from '@material-ui/core/Typography';
 import '../css/banner.css'
 
 export default class Banner extends PureComponent {
+
+  textAlignment(align){
+    if(align === 'left')
+      return "flex-start";
+    else if(align === 'right')
+      return "flex-end";
+    else
+      return "center";
+  }
+
+  renderIcon(icon){
+    if(icon !== undefined){
+      return(
+        <i className="material-icons md-24" style={{color: 'white'}}> {icon}</i>
+      );
+    }
+  }
+
+  renderButton(title, icon){
+    if(title !== undefined){
+      return(
+        <Grid item>
+          <Button variant="contained" size="large" classes={{root: 'banner-bt-root'}}>
+            {this.renderIcon(icon)}
+            {title}
+          </Button>
+        </Grid>
+      );
+    }
+  }
+
   render(){
     return(
       <div className="banner-container">
         <img src={this.props.img} id="banner-img" alt="banner"/>
-        <Grid container direction="column" justify='center' alignItems="center" id="banner-desc">
-          <Grid item>
-            <Typography className="banner-text" id="banner-title">Sports Recovery Lounge</Typography>
+        <Grid container
+          direction="column"
+          justify='center'
+          alignItems={this.textAlignment('right')}
+          style={{height: 'inherit', padding: '0 5%'}}>
+
+          <Grid container direction="column" justify='center' alignItems="flex-start"  id="banner-desc">
+            <Grid item>
+              <Typography className="banner-text" id="banner-title" component="div">BANNER TITLE</Typography>
+            </Grid>
+            <Grid item>
+              <Typography className="banner-text" id="banner-subtitle" gutterBottom>Banner description stuff might be multiple lines</Typography>
+            </Grid>
+            {this.renderButton("button text")}
           </Grid>
-          <Grid item>
-            <p className="banner-text" id="banner-subtitle">Sports Recovery Lounge description text or something</p>
-          </Grid>
-          <Grid item style={{position: 'absolute', bottom: '60px'}}>
-            <Button variant="outlined" size="large" classes={{root: 'banner-bt-root'}}>
-              <i className="material-icons md-24" style={{color: 'goldenrod'}}> calendar_today</i>
-              Make an Appointment
-            </Button>
-          </Grid>
+
         </Grid>
       </div>
     );
