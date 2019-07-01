2ea7b3405f19d5cf01c5e474fb775f55db772c8c
Jesse Martinez <jezze.04@gmail.com>
2019-07-01 02:40:25 -0500

    fixed footer which closes #4

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,16 +2,20 @@ import React, {PureComponent} from 'react';
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faPhoneAlt, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
 
 import '../css/footer.css';
 
 export default class Footer extends PureComponent {
 
   hoursData = [
-    {day: "Monday",     time: " 8:00 AM - 7:00 PM"},
-    {day: "Tueday",     time: " 8:00 AM - 7:00 PM"},
-    {day: "Wednesday",  time: "10:00 AM - 7:00 PM"},
-    {day: "Thursday",   time: "10:00 AM - 7:00 PM"},
+    {day: "Monday & Tuesday",     time: " 8:00 AM - 7:00 PM"},
+    // {day: "Tueday",     time: " 8:00 AM - 7:00 PM"},
+    {day: "Wednesday & Thursday",  time: "10:00 AM - 7:00 PM"},
+    // {day: "Thursday",   time: "10:00 AM - 7:00 PM"},
     {day: "Friday",     time: " 8:00 AM - 7:00 PM"},
     {day: "Saturday",   time: " 9:00 AM - 2:00 PM"},
     {day: "Sunday",     time: "CLOSED"},
@@ -20,13 +24,49 @@ export default class Footer extends PureComponent {
   renderHours(){
     return(
       <Grid item sm={4} container direction="column" id="hours-container">
-        <Typography variant="h5" gutterBottom>Hours of Operation</Typography>
+        <Typography variant="h5" className="footer-title" gutterBottom>Hours of Operation</Typography>
         {this.hoursData.map((day) =>
           <Grid item container direction="row">
-            <Grid item sm={4}>{day.day}</Grid>
-            <Grid item sm={8}>{day.time}</Grid>
+            <Grid item sm={5}>{day.day}</Grid>
+            <Grid item sm={4} style={{textAlign: 'right'}}>{day.time}</Grid>
           </Grid>
         )}
+      </Grid>
+    );
+  }
+
+  renderSocial(){
+    return(
+      <Grid item sm={2} container direction="column" id="social-container">
+        <Typography variant="h5" className="footer-title" gutterBottom>Follow Us!</Typography>
+        <Grid item container direction="row">
+          <Grid item sm={6}><FontAwesomeIcon icon={faFacebookSquare} size="3x" className="fa-icon" /></Grid>
+          <Grid item sm={6}><FontAwesomeIcon icon={faInstagram} size="3x" className="fa-icon" /></Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  renderContact(){
+    return(
+      <Grid item container sm={4} direction="row">
+        <Grid item sm={6} container direction="column" id="contact-container">
+          <Typography variant="h5" className="footer-title" gutterBottom>Call Us!</Typography>
+          <Grid item>
+            <FontAwesomeIcon icon={faPhoneAlt} size="2x" className="fa-icon" />
+            <Typography className="contact-text">+1 (956) 701-0173</Typography>
+          </Grid>
+        </Grid>
+        <Grid item sm={6} container direction="column" id="contact-container">
+          <Typography variant="h5" className="footer-title" gutterBottom>Visit Us!</Typography>
+          <Grid item>
+            <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" className="fa-icon" />
+            <Typography className="contact-text">
+              416 SHILOH RD. STE. 2B
+              LAREDO, TEXAS 78045
+            </Typography>
+          </Grid>
+        </Grid>
       </Grid>
     );
   }
@@ -34,14 +74,9 @@ export default class Footer extends PureComponent {
   render(){
     return(
       <Grid container direction="row" justify="space-evenly" id="footer-container">
-        {/* Hours of Operation */}
         {this.renderHours()}
-
-        {/* Social Links */}
-        <Grid item sm={4}></Grid>
-
-        {/* Contact + Location */}
-        <Grid item sm={4}></Grid>
+        {this.renderSocial()}
+        {this.renderContact()}
       </Grid>
     );
   }
