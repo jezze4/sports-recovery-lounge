95c8036646b2570f807c4ea42627004701b42f3c
Jesse Martinez <jezze.04@gmail.com>
2019-10-28 23:07:02 -0500

    appointments can be deleted from the schedule page

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,5 +1,7 @@
 import React, {PureComponent} from 'react';
 import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { Scheduler, WeekView, Appointments } from '@devexpress/dx-react-scheduler-material-ui';
 import { DateNavigator, Toolbar, TodayButton } from '@devexpress/dx-react-scheduler-material-ui';
 import { AppointmentTooltip } from '@devexpress/dx-react-scheduler-material-ui';
@@ -29,9 +31,9 @@ const appointmentContent = ({...props}) => {
 
 /* AppointmentTooltip */
 
-const tooltipHeader = ({...props}) => {
+const tooltipHeader = ({commandButtonIds, ...restProps}) => {
 
-  const {appointmentData} = props;
+  const {appointmentData} = restProps;
   return(
     <div style={{textAlign: 'center', padding: '8px 16px', background: '#5FBBFF', color: 'white'}}>
       <h2 style={{fontWeight: '500'}}>{appointmentData.username}</h2>
@@ -39,25 +41,18 @@ const tooltipHeader = ({...props}) => {
   );
 }
 
-const tooltipContent = ({...props}) => {
-  const {appointmentData} = props;
-  console.log(appointmentData);
-  return(
-    <div style={{padding: '0px 16px', textAlign: 'left'}}>
-      <p style={{fontSize: ".8em", marginBottom: '0px'}}>User ID: {appointmentData.userID}</p>
-      <h4 style={{fontWeight: "normal"}}>Email: {appointmentData.user}</h4>
-      <h4 style={{fontWeight: "normal"}}>Type: {appointmentData.type.toUpperCase()}</h4>
-      <h4 style={{fontWeight: "normal", marginBottom: '0px'}}>Length: {appointmentData.length} minutes</h4>
-      <AppointmentTooltip.Content {...props}/>
-    </div>
-  );
-}
 
 export default class Schedule extends PureComponent {
 
   state={
     appData: [],
-    dataFetched: false
+    dataFetched: false,
+    visible: false,
+  }
+
+  toggleVisibility = () => {
+    let visibilty = this.state.visible;
+    this.setState({visible: !visibilty})
   }
 
   componentDidMount(){
@@ -65,9 +60,15 @@ export default class Schedule extends PureComponent {
   }
 
   getAppointments(){
-    srl_db.collection("appointments").get()
-      .then(query => query.docs.map(doc => doc.data()))
-      .then(docs => this.setState({appData: docs}))
+
+    srl_db.collection("appointments").onSnapshot( snapshot => {
+      this.setState({appData: []})
+      snapshot.docs.forEach( doc => {
+        this.setState(prevState => ({
+          appData: [doc.data(), ...prevState.appData]
+        }))
+      })
+    })
   }
 
   render(){
@@ -92,13 +93,54 @@ export default class Schedule extends PureComponent {
             appointmentContentComponent={appointmentContent}
           />
           <AppointmentTooltip
-            showCloseButton
             headerComponent={tooltipHeader}
-            contentComponent={tooltipContent}
-            // showOpenButton
+            contentComponent={this.tooltipContent}
+            visible={this.state.visible}
+            onVisibilityChange={this.toggleVisibility}
           />
         </Scheduler>
       </Paper>
     );
+  }
+
+  tooltipContent = ({...props}) => {
+    const {appointmentData} = props;
+    return(
+      <div style={{padding: '0px 16px', textAlign: 'left'}}>
+        <p style={{fontSize: ".8em", marginBottom: '0px'}}>User ID: {appointmentData.userID}</p>
+        <h4 style={{fontWeight: "normal"}}>Email: {appointmentData.user}</h4>
+        <h4 style={{fontWeight: "normal"}}>Type: {appointmentData.type.toUpperCase()}</h4>
+        <h4 style={{fontWeight: "normal", marginBottom: '0px'}}>Length: {appointmentData.length} minutes</h4>
+        <AppointmentTooltip.Content {...props}/>
+        <Button
+          style={{width: 'calc(100% + 32px)', margin: '0 -16px'}}
+          variant="contained"
+          color="secondary"
+          size="large"
+          id="delete"
+          onClick={()=>this.handleDelete(appointmentData)}>
+          <DeleteIcon />
+        </Button>
+      </div>
+    );
+  }
+
+  handleDelete(data) {
+
+    let query = srl_db.collection("appointments")
+    .where("userID", "==", data.userID)
+    .where("startDate", "==", data.startDate);
+
+    query.get().then( snapshot => {
+      snapshot.forEach( doc => {
+        console.log("Deleting...")
+        this.toggleVisibility();
+        doc.ref.delete();
+      })
+    })
+    .then( alert("Appointment Deleted"))
+    .catch( error => {
+      console.log("Oops... something bad happened: " + error);
+    })
   }
 }
